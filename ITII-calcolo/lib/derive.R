6bd04ef47382b32a61c8cee472d661f4d5a32a03
Claudio <claudio.ortelli@gmail.com>
2012-01-01 23:51:25 +0100

    finito derive monomial

--- ITII-calcolo/lib/derive.R
+++ ITII-calcolo/lib/derive.R
@@ -6,30 +6,46 @@
 
 derive <- function(x,wrt="L") UseMethod("derive",x)
 
-derive.symbol <- function(x,wrt="L") {
-	# x: a symbol
+derive.monomial <- function(x,wrt="L") {
+	# x: a monomial
 	# wrt: "with respect to", i.e. the variable name wrt derive
 	
-	# returns a list with three slots: number, name and power
-	if (x$name!=wrt) return(create_monomial(number=0))
-	tmp <- create_symbol(name="L",power=x$power-1)
-	tmp1 <- create_symbols(tmp)
-	tmp2 <- create_monomial(number=x$power,symbols=tmp1)
-	return(tmp2)
+	symbols <- x$symbols
+	l.symbols <- length(symbols)
 	
-}
-
-
-derive.symbols <- function(x,wrt="L") {
-	# x: a symbols
-	# wrt: "with respect to", i.e. the variable name wrt derive
-	??? x <- compact(x)
-	# returns a list with three slots: number, name and power
-	if (x$name!=wrt) return(create_monomial(number=0))
-	tmp <- create_symbol(name="L",power=x$power-1)
-	tmp1 <- create_symbols(tmp)
-	tmp2 <- create_monomial(number=x$power,symbols=tmp1)
-	return(tmp2)
+	if (l.symbols==0) return(create_monomial(number=0))
+	
+	if (l.symbols==1) {
+		if (symbols[[1]]$name!=wrt) {
+			return(create_monomial(number=0))		
+		} else {
+			power <- symbols[[1]]$power
+			if (power==0) return(create_monomial(number=0))
+			
+			if (power==1) {
+				symbols[[1]] <- NULL
+				return(create_monomial(number=x$number,symbols=symbols,randoms=x$randoms))
+			}
+			number <- x$number*power
+			symbols[[1]]$power <- power-1
+			result <- create_monomial(number=number,symbols=symbols,randoms=x$randoms)
+			return(result)
+		}
+	}
+	
+	number <- x$number
+	symbols <- compact(x$symbols)
+	areDerived <- sapply(symbols,function(x,wrt){return(x$name==wrt)},wrt)
+	if (any(areDerived)) {
+		symbolsToDerive <- symbols[areDerived]
+		symbols[areDerived] <- NULL
+		tmpMonomial <- create_monomial(number=x$number,symbols=symbols,randoms=x$randoms)
+		tmpToDerive <- create_monomial(number=1,symbols=symbolsToDerive)
+		result <- tmpMonomial*derive(tmpToDerive)
+		return(result[[1]])
+	} else {
+		return(create_monomial(number=0))
+	}
 	
 }
 
--- ITII-calcolo/unitTests/t.derive/runit.derive.R
+++ ITII-calcolo/unitTests/t.derive/runit.derive.R
@@ -3,16 +3,43 @@
 # Author: claudio
 ###############################################################################
 
-test.derive.symbol <- function()  {
+
+test.derive.monomial <- function()  {
 	
-	# check 1: derive constant
-	a1 <- create_symbol(name="a1",power=4)
-	result <- derive(a1)
+	# check 1: deriva un semplice numero
+	monomial <- create_monomial(number=4)
+	result <- derive(monomial)
 	checkEquals(result,create_monomial(number=0))
 	
-	a1 <- create_symbol(name="L",power=4)
-	result <- derive(a1)
-	checkEquals(result$number,4)
-	checkEquals(result$symbols[[1]]$name,"L")
-	checkEquals(result$symbols[[1]]$power,3)
+	# check 2: deriva un semplice monomio senza "L"
+	monomial <- monomialFromString("5*a1^3")
+	result <- derive(monomial)
+	checkEquals(result,create_monomial(number=0))
+	
+	# check 3: deriva un semplice monomio con "L"
+	monomial <- monomialFromString("5*L^3")
+	result <- derive(monomial)
+	checkEquals(result,monomialFromString("15*L^2"))
+	
+	# check 4: deriva monomio lungo senza "L"
+	monomial <- monomialFromString("5*a1^3*b2^2")
+	result <- derive(monomial)
+	checkEquals(result,create_monomial(number=0))
+	
+	# check 5: deriva monomio lungo con "L"
+	monomial <- monomialFromString("5*a1^3*b2^2*L^12")
+	result <- derive(monomial)
+	checkEquals(result,monomialFromString("60*a1^3*b2^2*L^11"))
+
+	# check 6: deriva monomio semplice con "L^1"
+	monomial <- monomialFromString("5*L^1")
+	result <- derive(monomial)
+	checkEquals(result,monomialFromString("5"))
+	
+	# check 7: deriva monomio lungo con "L^1"
+	monomial <- monomialFromString("5*a1^3*b2^2*L^1")
+	result <- derive(monomial)
+	checkEquals(result,monomialFromString("5*a1^3*b2^2"))
 }
+
+
