9014f28c4b88e6ae68f9159f3e5ece9134112c19
Claudio Ortelli <claudio.ortelli@gmail.com>
2012-02-01 18:21:51 +0100

    ...

--- VAR/lib/mappaturaFondiOttimizzatore.R
+++ VAR/lib/mappaturaFondiOttimizzatore.R
@@ -5,11 +5,11 @@
 
 source("./odbc/connessioni.R")
 
-clienteFI <- "pippo76" # Euro fixed income
-clienteGec <- "pippo210" # Global economy
-clienteGeq <- "pippo53" # Global equity
+fondoFixedIncome <- "pippo76" # Euro fixed income
+fondoGlobalEconomy <- "pippo210" # Global economy
+fondoGlobalEquity <- "pippo53" # Global equity
 
-fetchDate <- "2011-12-30"
+fetchDate <- "2012-01-27"
 desiredFields <- c("Cliente",
 		"Strumento",
 		"Moneta",
@@ -19,50 +19,96 @@ desiredFields <- c("Cliente",
 		"ID_AAA","ID_strumento",
 		"Scadenza")
 
-datiFI  <- importDBPortfolioGeneraleDataFrame(c=clienteFI,f=fetchDate)[,desiredFields]
-datiGec <- importDBPortfolioGeneraleDataFrame(c=clienteGec,f=fetchDate)[,desiredFields]
-datiGeq <- importDBPortfolioGeneraleDataFrame(c=clienteGeq,f=fetchDate)[,desiredFields]
+datiFixedIncome  <- importDBPortfolioGeneraleDataFrame(c=fondoFixedIncome,f=fetchDate)[,desiredFields]
+datiGlobalEconomy <- importDBPortfolioGeneraleDataFrame(c=fondoGlobalEconomy,f=fetchDate)[,desiredFields]
+datiGlobalEquity <- importDBPortfolioGeneraleDataFrame(c=fondoGlobalEquity,f=fetchDate)[,desiredFields]
 
 # prendi repositoryAssetClassMapping
 repo <- create_repositoryAssetClassMapping()
-areMissing <- logical(nrow(datiFI))
 
-for (i in 1:nrow(datiFI)) {
-	areMissing[i] <- !any(is.element(datiFI[i,"Nome"],repo$assetClassMapping.df[["Nome"]]))
+# verifica che non ci siano posizioni che si ripetano nel repositoryAssetClassMapping
+lNames <- length(repo$assetClassMapping.df[["Nome"]])
+lUniqueNames <- length(unique(repo$assetClassMapping.df[["Nome"]]))
+if (!identical(lNames,lUniqueNames)) stop("Verifica doppioni nel file repositoryAssetClassMapping.csv")
+rownames(repo$assetClassMapping.df) <- repo$assetClassMapping.df[["Nome"]]
+
+
+newColumns <- function(desiredNames) {
+	# questa funzione restituisce le colonne assetClass e benchmark da aggiungere 
+	# alle colonne esistenti.
+	result <- repo$assetClassMapping.df[desiredNames,c("assetClass","benchmark")]
+	rownames(result) <- NULL
+	return(result)
+}
+
+aggregatePositions <- function(x) {
+	# x: il data.frame contenente le posizioni del portafoglio
+	# questa funziona calcola e raggruppa i pesi % rispetto ai benchmark 
+browser()	
+	# seleziona i nomi delle posizioni in portafoglio
+	currentPortfolioNames <- x[["Nome"]]
+	# determina le assetClass ed i benchmark corrispondenti
+	nuoveColonne.df <- newColumns(currentPortfolioNames)
+	# calcola il totale del portafoglio in CHF
+	total <- sum(x[,"ValoreMercatoMonetaCHF"])
+	# calcola i valori percentuali
+	x[["ValoreMercatoMonetaCHF"]] <- x[["ValoreMercatoMonetaCHF"]] / total
+	# aggiungi le nuove colonne ai dati
+	nuoveColonne.df <- cbind(nuoveColonne.df,x["Moneta"])
+	# determina le combinazioni per le quali aggregare
+	loopOver.df <- unique(nuoveColonne.df)
+	# applica il filtro
+	result <- tapply(x[["ValoreMercatoMonetaCHF"]],nuoveColonne.df,FUN=sum,simplify=TRUE)
+	# aggiungi il risultato al data.frame con i fattori <assetClass,benchmark,moneta>
+	result <- cbind(loopOver.df,Pesi=result[as.matrix(loopOver)])
+	rownames(result) <- NULL
+	return(result)
+}
+
+# ora Fixed Income
+areMissing <- logical(nrow(datiFixedIncome))
+
+for (i in 1:nrow(datiFixedIncome)) {
+	areMissing[i] <- !any(is.element(datiFixedIncome[i,"Nome"],repo$assetClassMapping.df[["Nome"]]))
 }
 
 fieldsNames <- c("ID_strumento","ID_AAA","Strumento","Moneta","Nome")
 if (any(areMissing)) {
-	missing <- datiFI[areMissing,fieldsNames]
+	missing <- datiFixedIncome[areMissing,fieldsNames]
 	write.csv(missing,file="./repositories/missingInstrForAssetClassMapping_FI.csv")
+} else {
+	resultFixedIncome <- aggregatePositions(datiFixedIncome)
 }
-
 
 # ora Global Economy
-areMissing <- logical(nrow(datiGec))
+areMissing <- logical(nrow(datiGlobalEconomy))
 
-for (i in 1:nrow(datiGec)) {
-	areMissing[i] <- !any(is.element(datiGec[i,"Nome"],repo$assetClassMapping.df[["Nome"]]))
+for (i in 1:nrow(datiGlobalEconomy)) {
+	areMissing[i] <- !any(is.element(datiGlobalEconomy[i,"Nome"],repo$assetClassMapping.df[["Nome"]]))
 }
 
 if (any(areMissing)) {
-	missing <- datiGec[areMissing,fieldsNames]
+	missing <- datiGlobalEconomy[areMissing,fieldsNames]
 	write.csv(missing,file="./repositories/missingInstrForAssetClassMapping_Gec.csv")
+} else {
+	resultGlobalEconomy <- aggregatePositions(datiGlobalEconomy)
 }
-
-
 
 # ora Global Equity
-areMissing <- logical(nrow(datiGeq))
+areMissing <- logical(nrow(datiGlobalEquity))
 
-for (i in 1:nrow(datiGeq)) {
-	areMissing[i] <- !any(is.element(datiGeq[i,"Nome"],repo$assetClassMapping.df[["Nome"]]))
+for (i in 1:nrow(datiGlobalEquity)) {
+	areMissing[i] <- !any(is.element(datiGlobalEquity[i,"Nome"],repo$assetClassMapping.df[["Nome"]]))
 }
 
 if (any(areMissing)) {
-	missing <- datiGeq[areMissing,fieldsNames]
+	missing <- datiGlobalEquity[areMissing,fieldsNames]
 	write.csv(missing,file="./repositories/missingInstrForAssetClassMapping_Geq.csv")
+} else {
+	resultGlobalEquity <- aggregatePositions(datiGlobalEquity)
 }
+
+
 
 
 
