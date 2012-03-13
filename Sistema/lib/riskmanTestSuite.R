69fea01397a1978871a05656a731f6a90e93830e
Claudio Ortelli <claudio.ortelli@gmail.com>
2012-03-13 17:48:36 +0100

    modificato file riskmanTestSuite.R

--- Sistema/lib/riskmanTestSuite.R
+++ Sistema/lib/riskmanTestSuite.R
@@ -8,7 +8,9 @@ setGeneric("runTest",def=function(x,...) standardGeneric("test"))
 
 setMethod("runTest",signature(x="TestSuite",positions="Positions"),
 		function(x,positions,valuationDate) { 
-			
+# nome della funzione originale, poi cancella ...			
+#testSingleRiskmanCheckFile <- function(fileName,inputDir,po,valuationDate) {
+#}			
 			# parsa il file della testSuite con i dati sui check
 			parsedTestSuite <- parser(x)
 			
@@ -21,7 +23,7 @@ setMethod("runTest",signature(x="TestSuite",positions="Positions"),
 				ownerPrintName <- paste(owner,collapse="_")
 				outputFileName <- paste(ownerPrintName,"_",valuationDate,".log",sep="")
 			} else {
-				stop("Error: manca owner o aggiorna codice")
+				stop("Error: missing owner or update your code")
 			}
 			
 			outputDir <- x@configLines[["outputDir"]]
@@ -34,8 +36,8 @@ setMethod("runTest",signature(x="TestSuite",positions="Positions"),
 			cat(paste("Out. directory:",outputDir),file=logFile,sep="\n",append=TRUE)
 			cat("\n",file=logFile,sep="\n",append=TRUE)
 			
-
-			results <- lapply(x@checkStrings,checkCheckStringOnPositions,positions,logFile)
+			# simultaneously apply all the checkStrings on the positions
+			results <- lapply(x@checkStrings,apply,positions,logFile)
 			checkResults <- extractFromList(results,"checkResult")
 			names(checkResults) <- extractFromList(results,"checkString")
 			
@@ -46,7 +48,7 @@ setMethod("runTest",signature(x="TestSuite",positions="Positions"),
 			cat(summary,file=logFile,sep="\n",append=TRUE)
 			close(con)
 			
-			# stampa il portafoglio in un file
+			# print the portfolio to a file
 			outputFileName <- paste("Portafoglio_",ownerPrintName,"_",valuationDate,".log",sep="")
 			outputDir <- testSuiteData@configLines[["outputDir"]]
 			logFile <- paste(outputDir,outputFileName,sep="/")
@@ -55,11 +57,11 @@ setMethod("runTest",signature(x="TestSuite",positions="Positions"),
 			con <- file(description=logFile,open="w")
 			cat("\n",file=logFile,sep="\n",append=TRUE)
 			cat("Portfolio structure:",file=logFile,sep="\n",append=TRUE)
-			portfolioStrings <- paste(positions$toString(),collapse="\n")
+			portfolioStrings <- paste(toString(positions),collapse="\n")
 			cat(portfolioStrings,file=logFile,sep="\n",append=TRUE)
 			close(con)
 			
-			# se c'è anche un solo FALSE crea un file di warning
+			# create a warning if a FALSE is detected
 			if (!all(checkResults)) {
 				outputFileName <- paste("warning_",valuationDate,".log",sep="")
 				outputDir <- testSuiteData@configLines[["outputDir"]]
@@ -74,100 +76,11 @@ setMethod("runTest",signature(x="TestSuite",positions="Positions"),
 )
 
 
-testSingleRiskmanCheckFile <- function(fileName,inputDir,po,valuationDate) {
-	
-	# parsa il file della testSuite con i dati sui check
-	parserTestSuite <- create_parserTestSuite()
-	parserTestSuite$importFile(paste(inputDir,"/",fileName,sep=""))
-	testSuiteData <- parserTestSuite$parseStrings()
-	
-	if (missing(valuationDate)) valuationDate <- Sys.Date()
-	# crea output infos
-	if (testSuiteData@configLines[["testSuiteKind"]]=="specific") { 
-		owner <- testSuiteData@configLines[["testTarget"]]
-		owner <- removeStartEndSpaces(unlist(strsplit(owner,",")))
-		ownerPrintName <- paste(owner,collapse="_")
-		outputFileName <- paste(ownerPrintName,"_",valuationDate,".log",sep="")
-	} else {
-		stop("Error: manca owner o aggiorna codice")
-	}
-	
-	outputDir <- testSuiteData@configLines[["outputDir"]]
-	logFile <- paste(outputDir,outputFileName,sep="/")
-#???	
-	# se po è di classe positions parsa po altrimenti estrai
-	# l'owner e segnala un errore
-	if (is(po,"Positions")) {
-		positions <- po
-	} else {
-		portfolio <- filterClassLists(po,"owner",owner)
-		if (length(portfolio)==0) {
-			con <- file(description=logFile,open="w")
-			cat(paste("Error: porfolio for owner",ownerPrintName,"is missing."),file=logFile,sep="\n",append=TRUE)
-			close(con)
-			return(FALSE)
-		}
-		if (length(portfolio)>1) {
-			portfolio <- join(portfolio[[1]],portfolio[-1])
-		}
-		
-		positions <- as(portfolio[[1]]@.Data,"Positions")
-		refCurrency <- portfolio[[1]]@referenceCurrency
-	}		
-	
-	con <- file(description=logFile,open="w")
-	cat(paste("Portfolio:",ownerPrintName),file=logFile,sep="\n",append=TRUE)
-	cat(paste("Input file:",fileName),file=logFile,sep="\n",append=TRUE)
-	cat(paste("Inp. directory:",inputDir),file=logFile,sep="\n",append=TRUE)
-	cat(paste("Out. directory:",outputDir),file=logFile,sep="\n",append=TRUE)
-	cat("\n",file=logFile,sep="\n",append=TRUE)
-# arrivato qui: devo verificare che la funzione checkCheckStringOnPositions funzioni
-	if (is.element("positions",class(po))) {
-		results <- lapply(testSuiteData@checkStrings,checkCheckStringOnPositions,positions,logFile)
-		checkResults <- extractFromList(results,"checkResult")
-		names(checkResults) <- extractFromList(results,"checkString")
-	} else {
-		results <- lapply(testSuiteData@checkStrings,checkCheckStringOnPositions,positions,logFile,refCurrency)			
-		checkResults <- extractFromList(results,"checkResult")
-		names(checkResults) <- extractFromList(results,"checkString")
-	}
-	
-	results.actualPercentage <- extractFromList(results,"actualPercentage")
-	summary <- paste(checkResults,": ", names(checkResults)," (actual ",results.actualPercentage,")",sep="",collapse="\n")
-	cat("Summary:",file=logFile,sep="\n",append=TRUE)
-	cat(summary,file=logFile,sep="\n",append=TRUE)
-	close(con)
-	
-	# stampa il portafoglio in un file
-	outputFileName <- paste("Portafoglio_",ownerPrintName,"_",valuationDate,".log",sep="")
-	outputDir <- testSuiteData@configLines[["outputDir"]]
-	logFile <- paste(outputDir,outputFileName,sep="/")
-	
-	
-	con <- file(description=logFile,open="w")
-	cat("\n",file=logFile,sep="\n",append=TRUE)
-	cat("Portfolio structure:",file=logFile,sep="\n",append=TRUE)
-	portfolioStrings <- paste(positions$toString(),collapse="\n")
-	cat(portfolioStrings,file=logFile,sep="\n",append=TRUE)
-	close(con)
-	
-	# se c'è anche un solo FALSE crea un file di warning
-	if (!all(checkResults)) {
-		outputFileName <- paste("warning_",valuationDate,".log",sep="")
-		outputDir <- testSuiteData@configLines[["outputDir"]]
-		logFile <- paste(outputDir,outputFileName,sep="/")
-		con <- file(description=logFile,open="at")
-		cat(ownerPrintName,file=logFile,sep="\n",append=TRUE)
-		close(con)
-	}
-	names(checkResults) <- NULL
-	return(checkResults)
-}
+
 runTestSuiteCollection <- function(testSuiteCollection,portfolios,valuationDate) {
 
 	runDirectory <- function(dir,portfolios,valuationDate) {
-		fileList <- list.files(path=dir, 
-				pattern=testSuite$testFileRegexp)
+		fileList <- list.files(path=dir,pattern=testSuite$testFileRegexp)
 		if(missing(valuationDate)) {
 			result <- lapply(fileList,testSingleRiskmanCheckFile,
 					inputDir=dir,po=portfolios)
