b283520ee98ab2eeae6d7df493699128a53e1ec3
Claudio <claudio.ortelli@gmail.com>
2012-04-12 17:20:38 +0200

    rimossi files

--- System/base/lib/riskmanOptions.R
+++
@@ -1,8 +0,0 @@
-# TODO: Add comment
-# 
-# Author: claudio
-###############################################################################
-
-
-riskmanOptions <- list()
-riskmanOptions[["eq.tolerance"]] <- 10^-6
