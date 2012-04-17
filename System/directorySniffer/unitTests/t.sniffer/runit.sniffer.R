76c940b37b67a9ce8d7d3d19450735919b6b3a3c
Claudio <claudio.ortelli@gmail.com>
2012-04-17 21:59:17 +0200

    iniziato sniffer

---
+++ System/directorySniffer/unitTests/t.sniffer/runit.sniffer.R
@@ -0,0 +1,15 @@
+# TODO: Add comment
+# 
+# Author: claudio
+###############################################################################
+
+
+test.shouldCreateSniffer <- function() {
+	
+	setClass("DirectorySniffer",representation(AbsoluteDirectoryPath="character",
+					newFileNames="character",oldFileNames="character"))
+	
+	
+	
+	#list.files
+}
