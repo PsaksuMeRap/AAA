a89b49dca7aea8c79d5b217195cd4b1344cc1118
Tega <claudio.ortelli@usi.ch>
2012-05-11 12:37:16 +0200

    riportato tutti i test su windows 7 usi ok

--- System/adviceManagement/lib/library.R
+++ System/adviceManagement/lib/library.R
@@ -5,10 +5,10 @@
 
 source("./adviceManagement/lib/logger.R") 
 source("./adviceManagement/lib/classDefinitions/Mail/Mail.R")
+source("./adviceManagement/lib/classDefinitions/Advisor/Advisor.R")
+source("./adviceManagement/lib/classDefinitions/Advisors/Advisors.R")
 source("./adviceManagement/lib/classDefinitions/Message/Message.R")
 source("./adviceManagement/lib/messageFactory.R") 
 source("./adviceManagement/lib/get_PID.R") 
-source("./adviceManagement/lib/classDefinitions/Advisor/Advisor.R")
-source("./adviceManagement/lib/classDefinitions/Advisors/Advisors.R")
 source("./adviceManagement/lib/classDefinitions/PostOffice/PostOffice.R") 
 source("./adviceManagement/lib/noLockOnNewAdvice.R") 
