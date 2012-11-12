5a1d004f6b716d6501697abbf42771cc191b13ee
Claudio Ortelli <claudio.ortelli@gmail.com>
2012-11-12 17:20:11 +0100

    modifica checkFiles multistrategy

--- System/base/lib/methods/securityFactory/securityFactory.R
+++ System/base/lib/methods/securityFactory/securityFactory.R
@@ -7,30 +7,5 @@
 
 setGeneric("securityFactory",def=function(origin,...) standardGeneric("securityFactory"))
 
-# Per ogni nuova classe di origin occorre creare un metodo securityFactory corrispondente
-#setMethod("securityFactory",signature(origin="AyrtonPosition"),
-#		function(origin,identifyOnly=FALSE) {
-#
-#			# define the function used to identify the position type, i.e. Equity, Bond, 
-#			# Hedge Fund, ...
-#		
-#			identifyInstrumentType <- function(origin) {
-#				# record: a list
-#				
-#				# create the repository of the instruments if not available
-#				if (!exists("instruments",envir=repositories,inherits=FALSE)) {
-#					eval(expression(instruments <- create_repositoryInstruments())
-#							,env=repositories)
-#				}
-#				
-#				securityType <- repositories$instruments$getInstrumentName(origin@ID_strumento)
-#				if (is.na(securityType)) {
-#					msg <- paste("Warning: the instrument with ID",
-#							origin@ID_strumento,"does not exist!")
-#					stop(msg)
-#				}
-#				return(securityType)
-#			}
-#)
 
 
