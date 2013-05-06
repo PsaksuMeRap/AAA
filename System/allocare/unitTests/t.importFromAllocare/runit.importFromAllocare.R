f70f6da06358fe054ce2c685da79ceacf9eaef97
Claudio Ortelli <claudio.ortelli@gmail.com>
2013-05-06 22:55:11 +0200

    creato test funzione importFromAllocare

--- System/allocare/unitTests/t.importFromAllocare/runit.importFromAllocare.R
+++ System/allocare/unitTests/t.importFromAllocare/runit.importFromAllocare.R
@@ -6,39 +6,52 @@
 
 test.shouldImportFile <- function() {
 	
-	file = "./allocare/unitTests/data/portafogli_allocare.txt"
-	data.df <- read.delim(file,header=TRUE,as.is=TRUE)
-	data.df[["Tree"]] <- NULL
-	data.df <- data.df[(data.df[["Asset.Category"]]!=""),]
-	
-	getRow <- function(i,df) { 
-		x <- df[i,,drop=TRUE]
+	importAllocarePortfolio <- function(file) {
 		
-		ratingSP <- x[["Rating.Standard...Poors"]]
-		ratingMoody <- x[["Rating.Moodys"]]
+		data.df <- read.delim(file,header=TRUE,as.is=TRUE)
 		
-		allocarePosition <- new("AllocarePosition",
-				Cliente=as.character(x[["Portfolio"]]),
-				Strumento=x[["Investment.Type"]],
-				Moneta=x[["Currency"]],
-				Saldo=as.numeric(gsub("'","",x[["Nominal.Quantity"]])),
-				NumeroValore=x[["Investment.Object.Identifier"]],
-				Nome=x[["Investment.Object.Name"]],
-				PrezzoMercato=as.numeric(gsub("'","",x[["Clean.Price"]])),
-				ValoreMercatoLocalCurrency=as.numeric(gsub("'","",x[["Value"]])),
-				ID_AAA=0,
-				ID_strumento=repositories$allocareInvestmentType$getId(x[["Investment.Type"]]),
-				rating=ifelse(ratingSP=="" | ratingSP=="NR",ratingMoody,ratingSP),
-				EconomicExposure=as.numeric(gsub("'","",x[["Eco.Exp"]])),
-				Underlying=x[["Underlying"]],
-				UnderlyingQuantity=as.numeric(gsub("'","",x[["Underlying.Quantity"]])),
-				OptionFeature=x[["Option.Feature"]]
-				
-		)
+		if (nrow(data.df)==0) return(new("AllocarePositions"))
 		
-		return(allocarePosition)
+		data.df[["Tree"]] <- NULL
+		data.df <- data.df[(data.df[["Asset.Category"]]!=""),]
+		
+		getRow <- function(i,df) { 
+			x <- df[i,,drop=TRUE]
+			
+			ratingSP <- x[["Rating.Standard...Poors"]]
+			ratingMoody <- x[["Rating.Moodys"]]
+			
+			allocarePosition <- new("AllocarePosition",
+					Cliente=as.character(x[["Portfolio"]]),
+					Strumento=x[["Investment.Type"]],
+					Moneta=x[["Currency"]],
+					Saldo=as.numeric(gsub("'","",x[["Nominal.Quantity"]])),
+					NumeroValore=x[["Investment.Object.Identifier"]],
+					Nome=x[["Investment.Object.Name"]],
+					PrezzoMercato=as.numeric(gsub("'","",x[["Clean.Price"]])),
+					ValoreMercatoLocalCurrency=as.numeric(gsub("'","",x[["Value"]])),
+					ID_AAA=0,
+					ID_strumento=repositories$allocareInvestmentType$getId(x[["Investment.Type"]]),
+					rating=ifelse(ratingSP=="" | ratingSP=="NR",ratingMoody,ratingSP),
+					EconomicExposure=as.numeric(gsub("'","",x[["Eco.Exp"]])),
+					Underlying=x[["Underlying"]],
+					UnderlyingQuantity=as.numeric(gsub("'","",x[["Underlying.Quantity"]])),
+					OptionFeature=x[["Option.Feature"]]
+			
+			)
+		}
+		result <- new("AllocarePositions",lapply(1:nrow(data.df),getRow,data.df))
+		return(result)
 	}
-
-	result <- lapply(1:nrow(data.df),getRow,data.df)
-	class(result) <- "allocarePositions"
+	
+	
+	# test empty file
+	filename = "./allocare/unitTests/data/portafogli_allocare_empty.txt"
+	result <- importAllocarePortfolio(filename)
+	checkEquals(length(result),0)
+	
+	# test non empty file
+	filename = "./allocare/unitTests/data/portafogli_allocare.txt"
+	result <- importAllocarePortfolio(filename)
+	checkEquals(length(result)>0,TRUE)
 }
