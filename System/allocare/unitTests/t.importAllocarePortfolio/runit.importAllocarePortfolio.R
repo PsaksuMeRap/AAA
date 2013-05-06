468c1c4538afdfd0a3c0e4a8eddfeeb328b36dfc
Claudio Ortelli <claudio.ortelli@gmail.com>
2013-05-06 22:57:29 +0200

    Creato funzione importAllocarePortfolio

---
+++ System/allocare/unitTests/t.importAllocarePortfolio/runit.importAllocarePortfolio.R
@@ -0,0 +1,18 @@
+# TODO: Add comment
+# 
+# Author: claudio
+###############################################################################
+
+
+test.shouldImportFile <- function() {
+	
+	# test empty file
+	filename = "./allocare/unitTests/data/portafogli_allocare_empty.txt"
+	result <- importAllocarePortfolio(filename)
+	checkEquals(length(result),0)
+	
+	# test non empty file
+	filename = "./allocare/unitTests/data/portafogli_allocare.txt"
+	result <- importAllocarePortfolio(filename)
+	checkEquals(length(result)>0,TRUE)
+}
