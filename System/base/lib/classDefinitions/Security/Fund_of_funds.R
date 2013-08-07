117ab2c11fd22ee15d138dbe6fc84327606896f7
tega <claudio.ortelli@gmail.com>
2013-08-07 18:03:40 +0200

    Aggiunto security Fund_of_funds

---
+++ System/base/lib/classDefinitions/Security/Fund_of_funds.R
@@ -0,0 +1,9 @@
+# TODO: Add comment
+# 
+# Author: claudio
+###############################################################################
+
+
+setClass("Fund_of_funds",representation(currency="Currency",name="character",id="Id"),contains="Security") 
+
+
