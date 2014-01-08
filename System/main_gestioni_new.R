063e4f9f69706d9fd4e26ada0fc21673e89815c4
tega <claudio.ortelli@gmail.com>
2014-01-08 15:32:02 +0100

    aggiornato profilo pippo223

--- System/main_gestioni_new.R
+++ System/main_gestioni_new.R
@@ -154,6 +154,7 @@ if (FALSE) {
 	
 	portfolios <- explodeAllPortfoliosByAllFunds(portfolios, only = c("GLOBAL ECONOMY"))
 	
+	## Marco devi cambiare i pesi qui!!!
 	portfolios <- explodeAndReplaceFundGlobalEquityAllPortfolios(portfolios,list(Eq=0.84,Fi=0.07))
 	
 	AyrtonTestSuite <- testSuiteFactory(testSuiteName="Clienti Ayrton",directories="./ClientiNew")
